34dc8cf5fc2b8f8e4d3d0c195e01a0ae96d7f56b
Austin Thao <austinthao@Austins-MacBook-Pro.local>
2022-10-04 22:28:49 -0700

    added protobuf structs

--- main.go
+++ main.go
@@ -1,40 +1,57 @@
 package main
 
 import (
-	"config/deploymentconfigurations"
 	"fmt"
-	"log"
 
-	"google.golang.org/protobuf/proto"
+	"github.com/austinthao5/golang_proto_test/config/deploymentconfigurations/deploymentEnv"
 )
 
 func main() {
 
 	fmt.Println("Hello World")
 
-	testEnv := deploymentconfigurations.Env{
-		name:  "testEnv",
-		value: "testValue",
+	testEnv := deploymentEnv.Env{
+		Name:  "testEnv",
+		Value: "testValue",
 	}
 
-	data, err := proto.Marshal(testEnv)
-	if err != nil {
-		log.Fatal("Marshaling error", err)
-	}
+	// data, err := proto.Marshal(&testEnv)
+	// if err != nil {
+	// 	log.Fatal("Marshaling error", err)
+	// }
 
-	fmt.Println(data)
+	fmt.Println(testEnv.Name)
 
-	testContainer := deploymentconfigurations.Containers{
-		name:    "Test",
-		image:   "testImage",
-		command: "yes",
-		env:     testEnv,
-	}
-	data_again, err := proto.Marshal(testContainer)
-	if err != nil {
-		log.Fatal("Marshaling error", err)
-	}
+	var strArray []*deploymentEnv.Env
 
-	fmt.Print(data_again)
+	var cmdArray []string
+
+	strArray = append(strArray, &testEnv)
+
+	cmdArray = append(cmdArray, "yes")
+
+	testContainer := deploymentEnv.Containers{
+		Name:    "Test",
+		Image:   "testImage",
+		Command: cmdArray,
+		Env:     strArray,
+	}
+	// data_again, err := proto.Marshal(&testContainer)
+	// if err != nil {
+	// 	log.Fatal("Marshaling error", err)
+	// }
+
+	var arr []*deploymentEnv.Containers
+
+	arr = append(arr, &testContainer)
+
+	fmt.Print(testContainer.Command)
+
+	// testDE := deploymentConfigurations.DeploymentEnvironment{
+	// 	Container: arr,
+	// }
+
+	// fmt.Print("\n------------\n")
+	// fmt.Print(testDE.Container)
 
 }
