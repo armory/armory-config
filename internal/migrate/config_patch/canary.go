28353d5115df751f6f314bf2d7029fd6266d6f9e
ichi0915 <ichi0915@gmail.com>
2022-10-25 17:22:10 -0500

    feat(config-patch): Update to use the real info for stats and canary

--- internal/migrate/config_patch/canary.go
+++ internal/migrate/config_patch/canary.go
@@ -1,47 +1,118 @@
 package config_patch
 
 import (
+	"strconv"
 	"strings"
 
+	"github.com/austinthao5/golang_proto_test/config/deploymentConfigurations/canary"
 	"github.com/austinthao5/golang_proto_test/internal/migrate/structs"
 )
 
 func GetCanary(KustomizeData structs.Kustomize) string {
+	str := ""
 
-	return GetCanaryConfig(KustomizeData)
+	if nil != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].Canary {
+		str = GetCanaryConfig(KustomizeData)
+	}
+	return str
 }
 
 func GetCanaryConfig(KustomizeData structs.Kustomize) string {
 	str := `
-	enabled: ` + "false" /*KustomizeData.Halyard.DeploymentConfiguration[KustomizeData.CurrentDeploymentPos].pubsub.Enable */ + `
-	serviceIntegrations:
-	- name: google
-	  enabled: false
-	  accounts: []
-	  gcsEnabled: false
-	  stackdriverEnabled: false
-	- name: prometheus
-	  enabled: false
-	  accounts: []
-	- name: datadog
-	  enabled: false
-	  accounts: []
-	- name: signalfx
-	  enabled: false
-	  accounts: []
-	- name: aws
-	  enabled: false
-	  accounts: []
-	  s3Enabled: false
-	- name: newrelic
-	  enabled: false
-	  accounts: []
-	reduxLoggerEnabled: true
-	defaultJudge: NetflixACAJudge-v1.0
-	stagesEnabled: true
-	templatesEnabled: true
-	showAllConfigsEnabled: true`
+		enabled: ` + strconv.FormatBool(KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].Canary.Enabled)
 
-	str = strings.Replace(str, "\t", "        ", -1)
+	if nil != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].Canary.ServiceIntegrations {
+		str += GetCanaryServiceIntegrations(KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].Canary)
+	}
+	str += `
+		reduxLogger: ` + strconv.FormatBool(KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].Canary.ReduxLogger) + `
+		defaultJudge: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].Canary.DefaultJudge + `
+		stagesEnabled: ` + strconv.FormatBool(KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].Canary.StagesEnabled) + `
+		templatesEnabled: ` + strconv.FormatBool(KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].Canary.TemplatesEnabled) + `
+		showAllConfigsEnabled: ` + strconv.FormatBool(KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].Canary.ShowAllConfigsEnabled)
+
+	str = strings.Replace(str, "\t", "    ", -1)
+
+	return str
+}
+
+func GetCanaryServiceIntegrations(canary *canary.Canary) string {
+	str := ""
+
+	// Accounts           []*CanaryAccounts `protobuf:"bytes,3,rep,name=accounts,proto3" json:"accounts,omitempty"`
+	if nil != canary.ServiceIntegrations {
+		str += `
+		serviceIntegrations:`
+
+		for _, serviceInteg := range canary.ServiceIntegrations {
+			str += `
+		  - name: ` + serviceInteg.Name + `
+		    enabled: ` + strconv.FormatBool(serviceInteg.Enabled) + `
+		    gcsEnabledd: ` + strconv.FormatBool(serviceInteg.GcsEnabled) +
+				getCanaryAccounts(serviceInteg.Accounts) + `
+		    stackdriverEnabled: ` + strconv.FormatBool(serviceInteg.StackdriverEnabled) + `
+		    s3Enabled: ` + strconv.FormatBool(serviceInteg.S3Enabled)
+		}
+	} else {
+		str += `
+		serviceIntegrations: []`
+	}
+
+	return str
+}
+
+func getCanaryAccounts(accounts []*canary.CanaryAccounts) string {
+	str := ""
+
+	if nil != accounts {
+		str += `
+		    accounts:`
+		for _, account := range accounts {
+			str += `
+		      - name: ` + account.Name + `
+		        project: ` + account.Project + `
+		        jsonPath: ` + account.JsonPath + `
+		        bucket: ` + account.Bucket + `
+		        bucketLocation: ` + account.BucketLocation + `
+		        rootFolder: ` + account.RootFolder +
+				getSupportedTypes(account.SupportedTypes) + `
+		        endpoint:
+		          baseUrl: ` + account.Endpoint.BaseUrl + `
+		        username: ` + account.Username + `
+		        password: ` + account.Password + `
+		        usernamePasswordFile: ` + account.UsernamePasswordFile + `
+		        apiKey: ` + account.ApiKey + `
+		        applicationKey: ` + account.ApplicationKey + `
+		        accessToken: ` + account.AccessToken + `
+		        defaultScopeKey: ` + account.DefaultScopeKey + `
+		        defaultLocationKey: ` + account.DefaultLocationKey + `
+		        region: ` + account.Region + `
+		        profileName: ` + account.ProfileName + `
+		        accessKeyId: ` + account.AccessKeyId + `
+		        secretAccessKey: ` + account.SecretAccessKey
+		}
+	} else {
+		str += `
+		    accounts: []`
+	}
+
+	return str
+}
+
+func getSupportedTypes(supTypes []string) string {
+	str := ""
+
+	if nil != supTypes {
+		str += `
+		          supportedTypes:`
+		for _, supType := range supTypes {
+			str += `
+		            ` + supType
+		}
+	} else {
+		str += `
+		          supportedTypes: []`
+	}
+
 	return str
 }
