0bf7defc94954a8b7eab641c94f8fb2a76f3200a
ichi0915 <ichi0915@gmail.com>
2022-10-25 10:04:42 -0500

    feat(persistent-storage): Update to use the real info for storage

--- internal/migrate/config_patch/persistentStorage.go
+++ internal/migrate/config_patch/persistentStorage.go
@@ -1,6 +1,7 @@
 package config_patch
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/austinthao5/golang_proto_test/internal/migrate/structs"
@@ -18,57 +19,63 @@ func GetPersistentStorage(KustomizeData structs.Kustomize) string {
 
 func GetPersistentStoreType(KustomizeData structs.Kustomize) string {
 	str := `
-	persistentStoreType: s3`
+	persistentStoreType: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.PersistentStoreType
 	str = strings.Replace(str, "\t", "        ", -1)
 	return str
-	// return `persistentStoreType: ` + KustomizeData.Halyard.DeploymentConfiguration[KustomizeData.CurrentDeploymentPos].PersistentStorage.persistentStoreType
 }
 
 func GetAzsStorage(KustomizeData structs.Kustomize) string {
 	str := ""
 
-	/*if nil != KustomizeData.Halyard.DeploymentConfiguration[KustomizeData.CurrentDeploymentPos].PersistentStorage.Azs {
-			str = `    azs: `
-		} else {
-			str = `    azs: {}
-	`
-		}*/
+	if nil != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Azs &&
+		"" != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Azs.StorageAccountName {
+		str = `  azs:
+			StorageAccountName: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Azs.StorageAccountName + `
+			StorageAccountKey: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Azs.StorageAccountKey + `
+			StorageContainerName: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Azs.StorageContainerName
+	} else {
+		str = `  azs: {}`
+	}
 
-	str = `
-	azs: {}`
-
-	str = strings.Replace(str, "\t", "        ", -1)
+	str = strings.Replace(str, "  ", "\n        ", -1)
+	str = strings.Replace(str, "\t\t\t", "          ", -1)
 	return str
 }
 
 func GetGcsStorage(KustomizeData structs.Kustomize) string {
 	str := ""
 
-	/*if nil != KustomizeData.Halyard.DeploymentConfiguration[KustomizeData.CurrentDeploymentPos].PersistentStorage.Gcs {
-			str = `    gcs: `
-		} else {
-			str = `    gcs: {}
-	`
-		}*/
+	if nil != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Gcs &&
+		"" != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Gcs.JsonPath {
+		str = `  gcs:
+			jsonPath: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Gcs.JsonPath + `
+			project: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Gcs.Project + `
+			bucket: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Gcs.Bucket + `
+			rootFolder: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Gcs.RootFolder + `
+			bucketLocation: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Gcs.BucketLocation
+	} else {
+		str = `  gcs: {}`
+	}
 
-	str = `
-	gcs:
-	  rootFolder: front50
-	  bucketLocation: ''`
-
-	str = strings.Replace(str, "\t", "        ", -1)
+	str = strings.Replace(str, "  ", "\n        ", -1)
+	str = strings.Replace(str, "\t\t\t", "          ", -1)
 	return str
 }
 
 func GetRedisStorage(KustomizeData structs.Kustomize) string {
 	str := ""
 
-	/*if nil != KustomizeData.Halyard.DeploymentConfiguration[KustomizeData.CurrentDeploymentPos].PersistentStorage.Redis {
-			str = `    redis: `
+	/*if nil != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.R &&
+		"" != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Gcs.JsonPath {
+			str = `  redis: `
 		} else {
-			str = `    redis: {}
+			str = `  redis: {}
 	`
-		}*/
+		}
+
+	str = strings.Replace(str, "  ", "\n        ", -1)
+	str = strings.Replace(str, "\t\t\t", "          ", -1)
+	return str*/
 
 	str = `
 	redis: {}`
@@ -80,39 +87,44 @@ func GetRedisStorage(KustomizeData structs.Kustomize) string {
 func GetS3Storage(KustomizeData structs.Kustomize) string {
 	str := ""
 
-	/*if nil != KustomizeData.Halyard.DeploymentConfiguration[KustomizeData.CurrentDeploymentPos].PersistentStorage.S3 {
-			str = `    s3: `
-		} else {
-			str = `    s3: {}
-	`
-		}*/
+	if nil != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.S3 &&
+		"" != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.S3.Bucket {
+		str = `  s3:
+			bucket: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.S3.Bucket + `
+			rootFolder: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.S3.RootFolder + `
+			region: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.S3.Region + `
+			pathStyleAccess: ` + strconv.FormatBool(KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.S3.PathStyleAccess) + `
+			accessKeyId: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.S3.AccessKeyId + `
+			secretAccessKey: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.S3.SecretAccessKey
+	} else {
+		str = `    s3: {}`
+	}
 
-	str = `
-	s3:
-	  bucket: spinnaker
-	  rootFolder: spinnaker
-	  region: us-west-2
-	  pathStyleAccess: false
-	  accessKeyId: XXXXX
-	  secretAccessKey: XXXXX`
-
-	str = strings.Replace(str, "\t", "        ", -1)
+	str = strings.Replace(str, "  ", "\n        ", -1)
+	str = strings.Replace(str, "\t\t\t", "          ", -1)
 	return str
 }
 
 func GetOracleStorage(KustomizeData structs.Kustomize) string {
 	str := ""
 
-	/*if nil != KustomizeData.Halyard.DeploymentConfiguration[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle {
-			str = `    oracle: `
-		} else {
-			str = `    oracle: {}
-	`
-		}*/
+	if nil != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle &&
+		"" != KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.BucketName {
+		str = `  oracle:
+			bucketName: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.BucketName + `
+			namespace: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.Namespace + `
+			compartmentId: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.CompartmentId + `
+			region: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.Region + `
+			userId: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.UserId + `
+			fingerprint: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.Fingerprint + `
+			sshPrivateKeyFilePath: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.SshPrivateKeyFilePath + `
+			privateKeyPassphrase: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.PrivateKeyPassphrase + `
+			tenancyId: ` + KustomizeData.Halyard.DeploymentConfigurations[KustomizeData.CurrentDeploymentPos].PersistentStorage.Oracle.TenancyId
+	} else {
+		str = `  oracle: {}`
+	}
 
-	str = `
-	oracle: {}`
-
-	str = strings.Replace(str, "\t", "        ", -1)
+	str = strings.Replace(str, "  ", "\n        ", -1)
+	str = strings.Replace(str, "\t\t\t", "          ", -1)
 	return str
 }
