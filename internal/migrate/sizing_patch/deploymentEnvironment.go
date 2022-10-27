c2788833b8196e2753022bbd7921e202812f614c
ichi0915 <ichi0915@gmail.com>
2022-10-27 11:42:44 -0500

    chore(clean): deploymentEnvironment.go file

--- internal/migrate/sizing_patch/deploymentEnvironment.go
+++ internal/migrate/sizing_patch/deploymentEnvironment.go
@@ -72,24 +72,6 @@ func GetCustomSizing(sizingReference *deploymentEnv.CustomSizing) string {
 		getServiceSizing(sizingReference.Orca, "orca") +
 		getServiceSizing(sizingReference.Rosco, "rosco") +
 		getServiceSizing(sizingReference.SpinFront50, "spin-front50")
-	//   spin-clouddriver:
-	//     replicas: 1     # Set the number of replicas (pods) to be created for this service.
-	//     # limits:
-	//     #   cpu: 2        # Set the kubernetes CPU limits for each pod. For reference a m5.xlarge EC2 instance has a capacity of 4 cpu.
-	//     #   memory: 4Gi   # Set the kubernetes memory limits for each pod. For reference a m5.xlarge EC2 instance has a capacity of 16Gi of memory.
-	//     # requests:
-	//     #   cpu: 500m
-	//     #   memory: 2Gi
-	//   # This applies sizings to only the service container and not to any sidecar
-	//   # containers running with service. (Use spin-<service> to include sidecars)
-	//   clouddriver:
-	//     limits:
-	//       cpu: 2        # Set the kubernetes CPU limits for each pod. For reference a m5.xlarge EC2 instance has a capacity of 4 cpu.
-	//       memory: 4Gi   # Set the kubernetes memory limits for each pod. For reference a m5.xlarge EC2 instance has a capacity of 16Gi of memory.
-	//     requests:
-	//       cpu: 500m
-	//       memory: 2Gi
-	//   deck:`
 
 	str = strings.Replace(str, "\t", "    ", -1)
 	return str
